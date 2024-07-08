22b15613b2875cbd325a2557ec6813f57862f383
azadcrt <104420501+azadcrt@users.noreply.github.com>
2024-07-08 12:32:45 +0200

    Add files via upload

--- emporter/en cours/serial_azad_one_ahrs.py
+++ emporter/en cours/serial_azad_one_ahrs.py
@@ -22,49 +22,49 @@ def send_data(obj_name, quaternion):
         client_socket.sendall(data.encode())
         print(f"Données envoyées avec succès au serveur Blender: {data}")
 
+known_eui64 = [
+    "62275C7830305C7831335C7861325C783030425C7831625C7864335C78613927",
+       "62275C7830305C7831335C7861325C783030425C7831625C7864343C27",
+       "62275C7830305C7831335C7861325C783030425C7831625C7864305C78653827",
+       "62275C7830305C7831335C7861325C783030425C7831625C7864335C78643427",
+       "62275C7830305C7831335C7861325C783030425C7831645C7861355C78633127"
+]
+object_names = ["upperarm_RL", "upperarm_R", "lowerarm_R", "clavicle_L", "clavicle_R"]
 
-known_eui64 = ["62275C7830305C7831335C7861325C783030425C7831625C7864335C78613927","62275C7830305C7831335C7861325C783030425C7831625C7864343C27","62275C7830305C7831335C7861325C783030425C7831625C7864305C78653827","62275C7830305C7831335C7861325C783030425C7831625C7864335C78643427","62275C7830305C7831335C7861325C783030425C7831645C7861355C78633127"]
-object_names = ["upperarm_RL","upperarm_R","lowerarm_R","clavicle_L","clavicle_R"]
-while True:
-    try:
-        with serial.Serial(port_name, baud_rate) as ser:
-            while True:
-                try:
-                    data = ser.readline().decode('ascii', errors='replace').strip()
-                    values = data.split()
-                    if len(values) >= 7:
-                        try:
-                            print(values[6])
-                            eui64_hex = eui64_to_hex(values[6].encode('ascii'))
-                            print(eui64_hex)
-                            if eui64_hex in known_eui64:
-                                ind = known_eui64.index(eui64_hex)
-                            
+try:
+    with serial.Serial(port_name, baud_rate) as ser:
+        while True:
+            try:
+                data = ser.readline().decode('ascii', errors='replace').strip()
+                values = data.split()
+                if len(values) >= 7:
+                    try:
+                        eui64_hex = eui64_to_hex(values[-2].encode('ascii'))
+                        print(values)
+                        if eui64_hex in known_eui64:
+                            ind = known_eui64.index(eui64_hex)
                             obj_name = object_names[ind]
-                            quaternion = Quaternion(float(values[2]),float(values[3]),float(values[4]),float(values[5]))
-                            quaternion_data = Quaternion.getFloor(quaternion)
-                            if etallonage[ind].x == 10:
-                                etallonage[ind].x = 20
-                            elif etallonage[ind].x == 20:
+                            quaternion = Quaternion(float(values[-6]), float(values[-5]), float(values[-4]), float(values[-3]))
+                            if etallonage[ind].q[1] == 10:
+                                etallonage[ind].q[1] = 20
+                            elif etallonage[ind].q[1] == 20:
                                 etallonage[ind] = quaternion.conj()
                             else:
-                                quaternion = quaternion * etallonage[ind]
+                                #quaternion = quaternion * etallonage[ind]
                                 send_data(obj_name, quaternion)
-                            #print(quaternion_data)
-                            
-                        except ValueError as e:
-                            print(f"Erreur de conversion des valeurs: {e}")
-                            #print(values)
-                    else:
-                        print("Données incorrectes:", values)
-                except UnicodeDecodeError as e:
-                    print("Erreur de décodage:", e)
-                    
-    except KeyboardInterrupt:
-        print("Programme arrêté par l'utilisateur.")
-        break
-    except serial.SerialException as e:
-        print(f"Erreur avec le port série: {e}")
-        time.sleep(1)
+                                print(obj_name, quaternion)
+                        else:
+                            print(f"EUI64 inconnu: {eui64_hex}")
+                    except ValueError as e:
+                        print(f"Erreur de conversion des valeurs: {e}")
+                else:
+                    print("Données incorrectes:", values)
+            except UnicodeDecodeError as e:
+                print("Erreur de décodage:", e)
+except KeyboardInterrupt:
+    print("Programme arrêté par l'utilisateur.")
+except serial.SerialException as e:
+    print(f"Erreur avec le port série: {e}")
+    time.sleep(1)
 
 
